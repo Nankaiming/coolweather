477c152f7fdf6aa20e720f17b6e6a4d105f7c6fa
Nankaiming <1017954423@qq.com>
2016-04-19 17:41:20 +0800

    icon modified

--- src/com/coolweather/app/receiver/AutoUpdateReceiver.java
+++ src/com/coolweather/app/receiver/AutoUpdateReceiver.java
@@ -5,7 +5,6 @@ import com.coolweather.app.service.AutoUpdateService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.sax.StartElementListener;
 
 public class AutoUpdateReceiver extends BroadcastReceiver {
 
